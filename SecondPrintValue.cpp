6d65ad438bec7162b3a4a9fb48cdbfcefee2bf42
Ravi Kumar Chaurasia <49302725+ravikashyap1118@users.noreply.github.com>
2020-03-05 00:21:35 +0530

    Create SecondPrintValue.cpp
    A : 0
    A  + B : 1
    A.B : 1 0
    now you see when a is null,no value store it give output default 0.

---
+++ SecondPrintValue.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b =1;
+    printf("\nValue Of A : %d ",a);
+    printf("\nValue Of A+B : %d ",a+b);
+    printf("\nA & B : %d %d",a,b);
+    return 0;
+}
